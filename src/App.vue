581c5e1dda7cb13a917677f0a0e14a1877d1e77c
Pavmois <58141080+Pavmois@users.noreply.github.com>
2023-09-21 15:14:23 +0300

    FlipBook added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
+    <router-link to="/">Главная</router-link> |
+    <router-link to="/about">Книга памяти</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/about">About</router-link>
@@ -19,9 +19,20 @@
   text-align: center;
   color: #7bff00;
 }
+html,
 body {
-  background: darkgray;
+  width: 100vw;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
 }
+body {
+  background: gray;
+  // background: linear-gradient(-45deg, #ffffff44, #664656, #a13c3c, #000000);
+  // background-size: 400% 400%;
+  // animation: gradient 5s ease infinite;
+}
+
 nav {
   background: rgb(109, 158, 170);
   padding: 30px;
@@ -33,6 +44,17 @@ nav {
     &.router-link-exact-active {
       color: #e00b0b;
     }
+  }
+}
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
   }
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Flipbook class="flipbook" :pages="pages" />
 </template>
 
-<style lang="scss" scoped>
-@import "@/styles/variables.scss";
-h1 {
-  color: $red;
+<script setup>
+import Flipbook from "flipbook-vue";
+import img1 from "@/assets/book/1.jpg";
+import img2 from "@/assets/book/2.jpg";
+import img3 from "@/assets/book/3.jpg";
+import img4 from "@/assets/book/4.jpg";
+import img5 from "@/assets/book/5.jpg";
+import img6 from "@/assets/book/6.jpg";
+
+// Массив изображений для модуля Flipbook
+const pages = [null, img1, img2, img3, img4, img5, img6];
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+.flipbook {
+  margin: 30px auto;
+  width: 80vw;
+  height: 80vh;
+  .flipbook-container {
+    .right::after {
+      display: block;
+      position: absolute;
+      top: 50%;
+      right: 0;
+      transform: translateY(-100%);
+      content: ">";
+      font-size: 40px;
+      line-height: 50px;
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      background: black;
+      z-index: 1;
+    }
+    .left::after {
+      display: block;
+      position: absolute;
+      top: 50%;
+      left: 0;
+      transform: translateY(-100%);
+      content: "<";
+      font-size: 40px;
+      line-height: 50px;
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      background: black;
+      z-index: 1;
+    }
+  }
 }
 </style>
