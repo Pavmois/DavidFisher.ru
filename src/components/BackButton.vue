3d540a626a314922019498ae7e9977fb7d4f17bc
Pavmois <58141080+Pavmois@users.noreply.github.com>
2023-09-29 23:08:19 +0300

    Back button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="back">
+    <router-link to="/">На главную</router-link>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.back {
+  position: fixed;
+  bottom: 15px;
+  left: 15px;
+  background: black;
+  border-radius: 20px;
+  padding: 10px;
+  font-size: 1rem;
+  a {
+    text-decoration: none;
+    color: white;
+  }
+}
+</style>
